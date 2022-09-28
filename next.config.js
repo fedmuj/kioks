efd1deb90eca5e3d8ec56c9d4d16212e88125bdd
Federico Mujica <federicomujica1@gmail.com>
2022-09-28 14:30:14 +0200

    config

--- next.config.js
+++ next.config.js
@@ -3,4 +3,11 @@ module.exports = {
   images: {
     domains: ['playsummit.sitecoresandbox.cloud', 'www.atcom.gr','www.eurobank.gr'],
   },
+  rules: {
+    //... other rules
+   // use 'off' instead of "warn" to disable the rule and the error message
+   'prettier/prettier': [   'warn',   {
+       endOfLine: 'auto',
+     },
+   ]}
 };
